dcc858cfd421fc5aa7b7147bf0a975d434f56a15
stepyarik395 <stepyarik395@gmail.com>
2023-10-17 21:51:55 +0300

    click__on_create__request

--- tests/login__page.spec.js
+++ tests/login__page.spec.js
@@ -1,11 +1,18 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('first__Test', async ({ page }) => {
+test('login__auditor', async ({ page }) => {
   await page.goto('https://certification-test.stg.disoft.dev/login');
   await page
     .getByLabel('Електронна адреса')
     .fill('secretary@certification.test.ua');
   await page.getByLabel('Пароль').nth(0).fill('dianetdev');
   await page.getByText('Увійти').click();
+  await expect(page).toHaveURL(
+    'https://certification-test.stg.disoft.dev/statements'
+  );
+  await page.getByText('Створити заявку').click();
+  await expect(page).toHaveURL(
+    'https://certification-test.stg.disoft.dev/statements/create'
+  );
 });
